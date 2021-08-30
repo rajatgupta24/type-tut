932cf13134035ea6849782bdb67556c7161251d5
rajat <rajat2411gupta@gmail.com>
2021-08-30 13:20:13 +0530

    Add scss

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import './s.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p className="header">
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
